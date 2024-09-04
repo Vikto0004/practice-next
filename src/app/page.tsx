2980f1b71e5270883040e0ed68fa7e71d776829f
Vikto0004 <vikto0004@gmail.com>
2024-09-04 17:08:07 +0200

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Hero from '@/components/Hero/Hero';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Image
+        src="/unnamed.png"
+        alt="Опис зображення"
+        width={500}
+        height={300}
+      />
     </>
   );
 }
