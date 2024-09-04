8c08a445f02645084151b9142a9c970166dada05
Vikto0004 <vikto0004@gmail.com>
2024-09-04 17:23:35 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import Cookies from 'js-cookie';
 
 function isStartsWith(request: NextRequest, path: string): boolean {
   return request.nextUrl.pathname.startsWith(path);
@@ -7,6 +8,8 @@ function isStartsWith(request: NextRequest, path: string): boolean {
 
 export const middleware = (request: NextRequest) => {
   const token = request.cookies.get('token')?.value;
+
+  Cookies.set('testing', 'dsfdsf', { expires: 7 });
 
   if (token && isStartsWith(request, '/login')) {
     return NextResponse.redirect(new URL('/contacts', request.url));
