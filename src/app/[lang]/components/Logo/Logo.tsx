061df7ecdbc5ba4eda004cd5a4cc7835dad684e0
Vikto0004 <vikto0004@gmail.com>
2024-09-11 12:57:06 +0200

    click for logo

--- src/app/[lang]/components/Logo/Logo.tsx
+++ src/app/[lang]/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import { Link } from '@/i18n/routing';
 import logoUa from '../../../../../public/svgs/logo_open-planet.svg';
 import logoEn from '../../../../../public/svgs/logo_open-planet-en.svg';
 
 export default function Logo({ lang }: { lang: string | string[] }) {
   return (
-    <>
+    <Link href="/">
       {lang === 'en' ? (
         <Image
           src={logoEn.src}
@@ -20,6 +21,6 @@ export default function Logo({ lang }: { lang: string | string[] }) {
           height={logoUa.height}
         />
       )}
-    </>
+    </Link>
   );
 }
